e75d38984b99eccb993120fc06c97805d7bf95b4
Richard Chu <rchu1211@gmail.com>
2022-07-15 09:07:19 -0700

    initial commit

---
+++ BasicsTest.java
@@ -0,0 +1,8 @@
+public class BasicsTest {
+    public static void main(String[] args) {
+        int[] nums = { 23, 234, 2345, -23, 42 };
+        LoopArray(nums);
+        MaxMinAverage(nums);
+        System.out.println(GetArraySum(nums));
+    }
+}
